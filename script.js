2f482c5af66dcf75cad9ac67abbec830391ab20e
Muhammad <muhammadbakhshuwein10@gmail.com>
2025-09-02 14:43:44 +0300

    text edit and color added

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "21 years old software engineering student";
+h2.textContent = "21 years young software engineering student";
 document.querySelector("body").appendChild(h2);
